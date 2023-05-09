ab7b47cf61b1e65416bf7faa0a55378c766a6d16
anton-s-ivanov <anton.s.ivanov@mail.ru>
2023-05-09 18:52:58 +0300

    Parcing Progress Redis

--- app/Http/Controllers/UploadExcelFileController.php
+++ app/Http/Controllers/UploadExcelFileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Rules\XLSXFormat;
 use App\Jobs\ParsingXLSX;
+use Illuminate\Support\Facades\Redis;
 
 
 class UploadExcelFileController extends Controller
@@ -23,10 +24,12 @@ class UploadExcelFileController extends Controller
 
         $fileContentArr = fastexcel()->import($request->file);
 
+        Redis::set('parsingProgress_'.auth()->user()->id,  0);
+
         foreach (collect($fileContentArr)->chunk(1000) as $chunk) {
             ParsingXLSX::dispatch($chunk);
         }
-
+            
         $message = 'File '. $request->file->getClientOriginalName() . ' uploaded ('. count($fileContentArr) . ' elems)';
         return back()->with('success', $message) ;
     }
--- app/Jobs/ParsingXLSX.php
+++ app/Jobs/ParsingXLSX.php
@@ -8,8 +8,9 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use \Illuminate\Support\Facades\DB;
 use App\Models\Row;
+use Illuminate\Support\Facades\Redis;
+
 
 class ParsingXLSX implements ShouldQueue
 {
@@ -30,12 +31,18 @@ class ParsingXLSX implements ShouldQueue
      */
     public function handle(): void
     {
+        // можно сделать вставку всего чанка через insert.
+        // будет быстрее, но точность учета прогресса будет кратна размеру чанка.
+        
         foreach($this->rowsArr as $row) {
             $createdRow = Row::create([
                 'import_id' => $row['id'],
                 'name' => $row['name'],
                 'date' => date_format($row['date'], 'Y-m-d'),
             ]);
+
+            Redis::incr('parsingProgress_'.auth()->user()->id);
         }
+        
     }
 }
